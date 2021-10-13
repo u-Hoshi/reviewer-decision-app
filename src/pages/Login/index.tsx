b37cb0975eff449faa15ee19ccb03c449c700348
u-Hoshi <yuta000707@yahoo.co.jp>
2021-10-13 23:04:59 +0900

    firebaseによるユーザ認証周りの実装

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,74 @@
+import { Button, Card, TextField, Typography } from '@material-ui/core';
+import { useLogin } from '../../hooks/Authentication/useLogin';
+import useStyles from './style';
 export const Login = () => {
-  return <h1>Login</h1>;
+  const styles = useStyles();
+  const { ref, error, loading, login } = useLogin();
+
+  return (
+    <Card className={styles.root} variant='outlined'>
+      <div className={`${styles.logo} ${styles.margin}`}></div>
+
+      <Typography className={styles.margin} component='h1' variant='h5'>
+        ログイン
+      </Typography>
+
+      {error.has('main') && (
+        <Typography className={styles.margin} color='error'>
+          {error.get('main')}
+        </Typography>
+      )}
+
+      <label className={`${styles.label} ${styles.margin}`}>
+        <Typography>メールアドレス</Typography>
+        <TextField
+          type='email'
+          required
+          size='small'
+          fullWidth
+          variant='outlined'
+          inputRef={ref.emailRef}
+          error={error.has('email')}
+          helperText={error.has('email') ? error.get('email') : ''}
+        />
+      </label>
+
+      <label className={`${styles.label} ${styles.margin}`}>
+        <Typography>パスワード</Typography>
+        <TextField
+          type='password'
+          required
+          size='small'
+          fullWidth
+          variant='outlined'
+          inputRef={ref.passwordRef}
+          error={error.has('password')}
+          helperText={error.has('password') ? error.get('password') : ''}
+        />
+      </label>
+
+      <div className={styles.margin}>
+        <Button
+          variant='contained'
+          color='primary'
+          disabled={loading}
+          onClick={login}
+        >
+          {loading ? 'ログイン中' : 'ログイン'}
+        </Button>
+      </div>
+
+      <div>
+        <Button href='/signup' color='primary'>
+          アカウント作成はこちら
+        </Button>
+      </div>
+
+      <div>
+        <Button href='/forget' color='primary'>
+          パスワードを忘れた場合はこちら
+        </Button>
+      </div>
+    </Card>
+  );
 };
