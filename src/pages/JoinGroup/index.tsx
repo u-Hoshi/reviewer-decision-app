22e330103bf2ab6bb1c52613b2e71af90d50aa82
koki-sys <soraisu7@gmail.com>
2021-10-23 19:13:26 +0900

    グループ参加画面のレイアウトを作成しました。

--- src/pages/JoinGroup/index.tsx
+++ src/pages/JoinGroup/index.tsx
@@ -1,3 +1,19 @@
+import { Grid, TextField, Button } from "@material-ui/core"
+
 export const JoinGroup = () => {
-  return <div>グループ参加画面</div>
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <h1>グループに参加</h1>
+          <TextField type="text" placeholder="グループ名" />
+          <br />
+          <br />
+          <Button variant="contained" size="large">
+            参加する
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
