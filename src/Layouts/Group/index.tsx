cf52bd8e1d36b500c52976d529cae57aa4bd72dc
u-Hoshi <yuta000707@yahoo.co.jp>
2021-10-23 15:00:44 +0900

    プロフィールページのレイアウトを作成

--- src/Layouts/Group/index.tsx
+++ src/Layouts/Group/index.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom"
 import { DashBoardHeader } from "../../templates/DashBoardHeader"
+import useStyles from "./style"
 
 export const GroupLayout = () => {
+  const styles = useStyles()
   return (
-    <div>
+    <>
       <DashBoardHeader />
-      <Outlet />
-    </div>
+      <div className={styles.main}>
+        <Outlet />
+      </div>
+    </>
   )
 }
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,3 +1,21 @@
-export const Profile = () => {
-  return <div>プロフィール画面</div>
+import { VFC } from "react"
+import useStyles from "./style"
+
+export const Profile: VFC = () => {
+  const styles = useStyles()
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.title}>プロフィール画面</h2>
+      <table className={styles.column}>
+        <tr>
+          <td>名前</td>
+          <td>サンプル</td>
+        </tr>
+        <tr>
+          <td>email</td>
+          <td>sample@sample.com</td>
+        </tr>
+      </table>
+    </div>
+  )
 }
