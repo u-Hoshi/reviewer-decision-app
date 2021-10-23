cdb3df8f21313e3b46c7ecb612c328a23a6f1b0f
koki-sys <soraisu7@gmail.com>
2021-10-23 14:31:55 +0900

    グループ作成画面を追加しました

--- src/pages/CreateGroup/index.tsx
+++ src/pages/CreateGroup/index.tsx
@@ -1,3 +1,21 @@
+import { Grid, Button, Input } from "@material-ui/core"
+
 export const CreateGroup = () => {
-  return <div>グループ作成画面</div>
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <h1>グループ新規作成</h1>
+          <Input type="text" placeholder="グループ名" />
+          <br />
+          <Input type="text" placeholder="Slackのwebhook" />
+          <br />
+          <br />
+          <Button variant="contained" size="large" href="/signup">
+            グループ作成
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
