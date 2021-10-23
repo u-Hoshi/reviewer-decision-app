bd2a1cedfb22e0df19ff4cb8b8513179d8f9e429
koki-sys <soraisu7@gmail.com>
2021-10-23 18:01:54 +0900

    エラー処理と、作成成功後の遷移を完成させました

--- src/hooks/Group/useCreate/index.ts
+++ src/hooks/Group/useCreate/index.ts
@@ -1,54 +1,74 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import { useNavigate } from "react-router"
 import { useInsertGroupMutation } from "../../../utils/graphql/generated"
+import { SetErrorFn, useCreateHelper } from "../useCreateHelper"
 
 export const useCreate = () => {
-    const nameRef = useRef<HTMLInputElement>(null)
-    const slackUrlRef = useRef<HTMLInputElement>(null)
+  const nameRef = useRef<HTMLInputElement>(null)
+  const slackUrlRef = useRef<HTMLInputElement>(null)
 
-    const [error, setError] = useState<Error>()
+  const navigate = useNavigate()
 
-    const [loading, setLoading] = useState<boolean>(false)
+  const [insertMutation, { error: apolloError }] = useInsertGroupMutation()
 
-    const [insertMutation, {error: apploError}] = useInsertGroupMutation()
+  const formValidation = (setError: SetErrorFn) => {
+    let invalidValidation = false
 
-    const create = async () => {
-        if(
-            !nameRef.current?.value ||
-            !slackUrlRef.current?.value
-        ) {
-            // エラー処理を入れる
-            return
-        }
-
-        try {
-            setLoading(true)
-
-            const apolloResponse = await insertMutation({
-                variables: {
-                    name: nameRef.current.value,
-                    slackUrl: slackUrlRef.current.value,
-                    responsible_id: 1
-                }
-            })
-            if(apolloResponse.data?.insert_group_one?.id){
-                // リダイレクト
-            } else {
-                throw new Error("グループ新規作成に失敗しました。")
-            }
-        } catch(error) {
-            setError(error)
-        } finally {
-            setLoading(false)
-        }
-
-        return {
-            ref: {
-                nameRef,
-                slackUrlRef
-            },
-            create,
-            error,
-            loading
-        }
+    // Nameフォームのバリデーションチェック
+    // 今回はシンプルにするために、入力が空でないかだけ確認する
+    if (!nameRef.current?.value) {
+      setError("name", "名前が入力されていません。")
+      invalidValidation = true
     }
+
+    // Emailフォームのバリデーションチェック
+    // 今回はシンプルにするために、入力が空でないかだけ確認する
+    if (!slackUrlRef.current?.value) {
+      setError("slackurl", "SlackのURLを入力してください。")
+      invalidValidation = true
+    }
+
+    // バリデーションが有効か無効化を返す
+    return invalidValidation
+  }
+
+  const create = async () => {
+    if (!nameRef.current?.value || !slackUrlRef.current?.value) {
+      // エラー処理を入れる
+      return
+    }
+
+    const apolloResponse = await insertMutation({
+      variables: {
+        name: nameRef.current.value,
+        slackUrl: slackUrlRef.current.value,
+        responsible_id: 1,
+      },
+    })
+
+    if (apolloResponse.data?.insert_group_one?.id) {
+        
+      navigate(`/${apolloResponse.data?.insert_group_one.id}`)
+    } else {
+      throw new Error("グループ新規作成に失敗しました。")
+    }
+  }
+
+  const { createExecute, error, setErrorHandler, loading } = useCreateHelper(create, formValidation)
+
+  useEffect(() => {
+    if (apolloError?.message) {
+      setErrorHandler("main", apolloError.message)
+    }
+  }, [apolloError])
+
+  return {
+    ref: {
+      nameRef,
+      slackUrlRef,
+    },
+    create: createExecute,
+    error,
+    loading,
+  }
 }
--- src/pages/CreateGroup/index.tsx
+++ src/pages/CreateGroup/index.tsx
@@ -1,20 +1,38 @@
-import { Grid, Button, Input } from "@material-ui/core"
+import { Grid, Button, Input, Typography, TextField } from "@material-ui/core"
 import useStyles from "./style"
+import { useCreate } from "../../hooks/Group/useCreate"
 
 export const CreateGroup = () => {
+  const { ref, error, loading, create } = useCreate()
+
   const styles = useStyles()
   return (
     <div>
+      {error.has("main") && <Typography color="error">{error.get("main")}</Typography>}
       <Grid container spacing={2} className={styles.root}>
         <Grid item xs={12}>
           <h1>グループ新規作成</h1>
-          <Input type="text" placeholder="グループ名" className={styles.groupname} />
+          <TextField
+            type="text"
+            placeholder="グループ名"
+            className={styles.groupname}
+            inputRef={ref.nameRef}
+            error={error.has("name")}
+            helperText={error.has("name") ? error.get("name") : ""}
+          />
           <br />
-          <Input type="text" placeholder="Slackのwebhook" className={styles.slackurl}/>
+          <TextField
+            type="text"
+            placeholder="Slackのwebhook"
+            className={styles.slackurl}
+            inputRef={ref.slackUrlRef}
+            error={error.has("slackurl")}
+            helperText={error.has("slackurl") ? error.get("slackurl") : ""}
+          />
           <br />
           <br />
-          <Button variant="contained" size="large" href="/signup" className={styles.submit}>
-            グループ作成
+          <Button variant="contained" size="large" onClick={create} className={styles.submit} disabled={loading}>
+          {loading ? "作成中です..." : "グループ作成"}
           </Button>
         </Grid>
       </Grid>
