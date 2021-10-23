2c73a8d53ddbcc6a2cbe003f0360713fd8ea3484
koki-sys <soraisu7@gmail.com>
2021-10-23 15:23:53 +0900

    スタイリングをしました。

--- src/pages/CreateGroup/index.tsx
+++ src/pages/CreateGroup/index.tsx
@@ -1,17 +1,19 @@
 import { Grid, Button, Input } from "@material-ui/core"
+import useStyles from "./style"
 
 export const CreateGroup = () => {
+  const styles = useStyles()
   return (
     <div>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} className={styles.root}>
         <Grid item xs={12}>
           <h1>グループ新規作成</h1>
-          <Input type="text" placeholder="グループ名" />
+          <Input type="text" placeholder="グループ名" className={styles.groupname} />
           <br />
-          <Input type="text" placeholder="Slackのwebhook" />
+          <Input type="text" placeholder="Slackのwebhook" className={styles.slackurl}/>
           <br />
           <br />
-          <Button variant="contained" size="large" href="/signup">
+          <Button variant="contained" size="large" href="/signup" className={styles.submit}>
             グループ作成
           </Button>
         </Grid>
