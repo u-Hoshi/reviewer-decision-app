ca616d2f2896f5707207edd365b291bc848d832b
koki-sys <soraisu7@gmail.com>
2021-10-23 19:18:03 +0900

    スタイリングをしました。

--- src/pages/JoinGroup/index.tsx
+++ src/pages/JoinGroup/index.tsx
@@ -1,15 +1,17 @@
 import { Grid, TextField, Button } from "@material-ui/core"
+import useStyle from "./style"
 
 export const JoinGroup = () => {
+  const styles = useStyle()
   return (
     <div>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} className={styles.root}>
         <Grid item xs={12}>
           <h1>グループに参加</h1>
-          <TextField type="text" placeholder="グループ名" />
+          <TextField type="text" placeholder="グループ名" className={styles.groupname} />
           <br />
           <br />
-          <Button variant="contained" size="large">
+          <Button variant="contained" size="large" className={styles.submit}>
             参加する
           </Button>
         </Grid>
---
+++ src/pages/JoinGroup/style.ts
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core"
+
+export default makeStyles({
+  root: {
+    marginTop: "10%",
+    textAlign: "center",
+  },
+  groupname: {
+    margin: "1% 0",
+  },
+  submit: {
+    margin: "1% 0",
+    backgroundColor: "#6F86FF",
+    color: "#FFFFFF",
+    "&:hover": {
+      backgroundColor: "#6376DB",
+    },
+  },
+})
