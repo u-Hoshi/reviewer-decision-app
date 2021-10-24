f20782f0abf7fa11670ce566dfee9c97de98643d
u-Hoshi <yuta000707@yahoo.co.jp>
2021-10-24 11:00:16 +0900

    グループメンバーの表を作成

--- src/pages/GroupMember/index.tsx
+++ src/pages/GroupMember/index.tsx
@@ -1,3 +1,52 @@
-export const GroupMember = () => {
-  return <div>グループメンバー画面</div>
+import {
+  Button,
+  Modal,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@material-ui/core"
+import { VFC } from "react"
+
+function createData(name: string, email: string, created_at: string) {
+  return { name, email, created_at }
+}
+
+const rows = [
+  createData("Frozen yoghurt", "hoge@sample.com", "2021-10-18"),
+  createData("Ice cream sandwich", "foo@sample.com", "2021-10-18"),
+]
+
+export const GroupMember: VFC = () => {
+  return (
+    <TableContainer component={Paper}>
+      <Table style={{ minWidth: "650px" }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>名前</TableCell>
+            <TableCell align="center">メールアドレス</TableCell>
+            <TableCell align="center">登録日</TableCell>
+            <TableCell align="center">役割変更</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name} style={{}}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="center">{row.email}</TableCell>
+              <TableCell align="center">{row.created_at}</TableCell>
+              <TableCell align="center">
+                <Button>変更</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
